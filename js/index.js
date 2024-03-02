2c278a1462495c643ab06a1d663fbe01b3f5a563
code2web <125441138+code2web@users.noreply.github.com>
2024-03-02 10:55:30 +0600

    Delete js directory

--- js/index.js
+++
@@ -1,6 +0,0 @@
-/*	for mobile menu*/
-const mobileNav = document.querySelector(".mobileNavberBtn");
-const headerElem = document.querySelector(".header");
-mobileNav.addEventListener("click", () => {
-	headerElem.classList.toggle("active");
-});
