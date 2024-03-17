435917b85eb54d285cb80b74720b96bdfdff9a90
code2web <125441138+code2web@users.noreply.github.com>
2024-03-17 17:27:26 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,30 @@
+/*	for mobile menu*/
+const mobileNav = document.querySelector(".mobileNavberBtn");
+const headerElem = document.querySelector(".mobileNavbarDisplay");
+// const headerElem = document.querySelector(".header");
+mobileNav.addEventListener("click", () => {
+	headerElem.classList.toggle("active");
+});
+
+const headerElemEach = document.querySelectorAll(".linkNavDiv");
+headerElemEach.forEach((elem) => {
+	elem.addEventListener("click", () => {
+		document.querySelector(".active")?.classList.remove("active");		
+	});
+});
+/*	for mobile menu*/
+
+/*searchHover mobile*//*searchHover mobile*/
+const searchIcon = document.querySelector('.searchIcon');
+const searchHover = document.querySelector('.searchHover');
+const mobileNavbarDisplay = document.querySelector('.mobileNavbarDisplay');
+const searchHoverClose = document.querySelector('.searchHoverClose');
+searchIcon.addEventListener('click', () => {
+	searchHover.style.display= 'block';
+	mobileNavbarDisplay.style.display= 'none';
+});
+searchHoverClose.addEventListener('click', () => {
+	searchHover.style.display= 'none';
+	mobileNavbarDisplay.style.display= 'block';
+});
+/*searchHover mobile*//*searchHover mobile*/
